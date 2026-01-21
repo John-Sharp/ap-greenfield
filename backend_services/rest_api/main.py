17ea736092c7620d5d68793e0aab23b7ed6c8b04
John Sharp <john.sharp@accessia.com>
2026-01-21 11:36:05 +0000

    Address #6 by doing something

--- backend_services/rest_api/main.py
+++ backend_services/rest_api/main.py
@@ -1,7 +1,7 @@
 import requests
 
 def main():
-    print("this is the rest api")
+    print("this is the rest api IMPROVED")
     print("Requests version:", requests.__version__)
 
 if __name__ == "__main__":
